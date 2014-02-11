8bcdae73c685330888ae2ff26f78deae25e05846
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2014-02-11 19:03:09 +0200

    Add Leiningen profiles.clj

---
+++ .lein/profiles.clj
@@ -0,0 +1,7 @@
+{:user {:plugins [[lein-ring "0.8.10"]
+                  [lein-midje "3.1.3"]
+                  [lein-ancient "0.5.4"]
+                  [lein-cloverage "1.0.2"]
+                  [lein-droid "0.2.2"]
+                  [lein-kibit "0.0.8"]
+                  [jonase/eastwood "0.1.0"]]}}
