5fbbb031715d6dad02fe853e8e05256ae1dd3bf2
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2014-06-22 14:31:50 +0300

    Update Leiningen plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,8 @@
-{:user {:plugins [[lein-ring "0.8.10"]
+{:user {:plugins [[lein-ring "0.8.11"]
                   [lein-midje "3.1.3"]
-                  [lein-ancient "0.5.4"]
+                  [lein-ancient "0.5.5"]
                   [lein-cloverage "1.0.2"]
-                  [lein-droid "0.2.2"]
+                  [lein-droid "0.2.3"]
                   [lein-kibit "0.0.8"]
-                  [jonase/eastwood "0.1.0"]]}}
+                  [jonase/eastwood "0.1.4"]
+                  [cider/cider-nrepl "0.7.0-SNAPSHOT"]]}}
