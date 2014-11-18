73a9f1f3362b422ff659ab1b777675eaabc20301
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2014-11-18 13:28:22 +0200

    Update lein-ring to 0.8.13

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[lein-ring "0.8.12"]
+{:user {:plugins [[lein-ring "0.8.13"]
                   [lein-ancient "0.5.5"]
                   [lein-cloverage "1.0.2"]
                   [lein-droid "0.2.3"]
