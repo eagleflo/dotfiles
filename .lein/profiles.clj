a4c97cda9ce4d2f7462bf133d12a944442d0ca1a
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2014-11-22 03:53:16 +0200

    Update eastwood to 0.2.0

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,5 +3,5 @@
                   [lein-cloverage "1.0.2"]
                   [lein-droid "0.2.3"]
                   [lein-kibit "0.0.8"]
-                  [jonase/eastwood "0.1.4"]
+                  [jonase/eastwood "0.2.0"]
                   [cider/cider-nrepl "0.8.1"]]}}
