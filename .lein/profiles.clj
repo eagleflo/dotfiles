80e3c820de9c9212cbd3393aad57fec29e4083db
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2014-11-22 03:13:19 +0200

    Update cider-nrepl to 0.8.1

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,4 +4,4 @@
                   [lein-droid "0.2.3"]
                   [lein-kibit "0.0.8"]
                   [jonase/eastwood "0.1.4"]
-                  [cider/cider-nrepl "0.8.0-SNAPSHOT"]]}}
+                  [cider/cider-nrepl "0.8.1"]]}}
