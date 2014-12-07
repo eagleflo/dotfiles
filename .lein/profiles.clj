239596a906da622279ae39085fb214bcd18d0df7
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2014-12-08 00:45:50 +0200

    Add lein-typed to profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,5 +3,6 @@
                   [lein-cloverage "1.0.2"]
                   [lein-droid "0.2.3"]
                   [lein-kibit "0.0.8"]
+                  [lein-typed "0.3.5"]
                   [jonase/eastwood "0.2.0"]
                   [cider/cider-nrepl "0.8.1"]]}}
