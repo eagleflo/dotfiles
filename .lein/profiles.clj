93f2590c931e36bed00884244514b02079a3d33c
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2015-03-16 18:08:43 +0200

    Update Leiningen plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [[lein-ring "0.9.1"]
-                  [lein-ancient "0.6.2"]
+                  [lein-figwheel "0.2.3-SNAPSHOT"]
+                  [lein-ancient "0.6.3"]
                   [lein-kibit "0.0.8"]
                   [lein-typed "0.3.5"]
                   [jonase/eastwood "0.2.1"]
