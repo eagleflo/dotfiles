057418e2d8b4c7881e61ae4b11d4591556216e6b
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2015-11-14 22:47:59 +0200

    Update lein-figwheel

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [[lein-ring "0.9.7"]
                   [lein-ancient "0.6.8"]
-                  [lein-figwheel "0.4.1"]
+                  [lein-figwheel "0.5.0-1"]
                   [lein-bikeshed "0.2.0"]
                   [lein-kibit "0.1.2"]
                   [lein-typed "0.3.5"]
