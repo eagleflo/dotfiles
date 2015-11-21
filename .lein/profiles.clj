8080a43d03b964d0f2302be3d49b190289081e5b
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2015-11-21 02:41:23 +0200

    Move CIDER to :repl profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,7 +4,7 @@
                   [lein-bikeshed "0.2.0"]
                   [lein-kibit "0.1.2"]
                   [lein-typed "0.3.5"]
-                  [jonase/eastwood "0.2.1"]
-                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.10"]
+                  [jonase/eastwood "0.2.1"]]}
+ :repl {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [acyclic/squiggly-clojure "0.1.4"]]}}
