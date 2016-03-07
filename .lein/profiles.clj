15724b3495fe6678dc7c4e3e6a653ea01b44249d
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2016-03-07 15:08:47 +0200

    Update Leiningen plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,10 +1,10 @@
 {:user {:plugins [[lein-ring "0.9.7"]
                   [lein-ancient "0.6.8"]
-                  [lein-figwheel "0.5.0-1"]
+                  [lein-figwheel "0.5.0-6"]
                   [lein-bikeshed "0.2.0"]
                   [lein-kibit "0.1.2"]
                   [lein-typed "0.3.5"]
-                  [jonase/eastwood "0.2.1"]]}
+                  [jonase/eastwood "0.2.3"]]}
  :repl {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [acyclic/squiggly-clojure "0.1.4"]]}}
