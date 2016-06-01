5bacfe1c73960181a8386ca48e28f832b4ac5c6f
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2016-06-01 17:18:10 +0300

    Remove troublesome lein plugins
    These were interfering with "lein new".

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,4 @@
 {:user {:plugins [[lein-ring "0.9.7"]
-                  [lein-ancient "0.6.10"]
-                  [lein-figwheel "0.5.2"]
                   [lein-bikeshed "0.3.0"]
                   [lein-kibit "0.1.2"]
                   [lein-typed "0.3.5"]
