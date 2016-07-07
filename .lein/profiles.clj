a69b994634436d4ce49e1bacfeb616e4f12a6176
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2016-07-07 10:28:06 +0300

    CIDER injects its Leiningen dependencies

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,5 +3,4 @@
                   [lein-bikeshed "0.3.0"]
                   [lein-kibit "0.1.2"]
                   [lein-typed "0.3.5"]
-                  [jonase/eastwood "0.2.3"]]}
- :repl {:plugins [[cider/cider-nrepl "0.12.0"]]}}
+                  [jonase/eastwood "0.2.3"]]}}
