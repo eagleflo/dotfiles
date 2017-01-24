059f734f61e217dae636dd1a3cda6213cdea484c
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2017-01-24 19:14:40 +0200

    Update lein plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,6 @@
-{:user {:plugins [[lein-ring "0.9.7"]
+{:user {:plugins [[lein-ring "0.10.0"]
                   [lein-ancient "0.6.10"]
-                  [lein-bikeshed "0.3.0"]
-                  [lein-kibit "0.1.2"]
+                  [lein-bikeshed "0.4.1"]
+                  [lein-kibit "0.1.3"]
                   [lein-typed "0.3.5"]
                   [jonase/eastwood "0.2.3"]]}}
