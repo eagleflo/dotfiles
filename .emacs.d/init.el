603fc2287703042ee7485e2f787684cc364df2a7
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2017-10-16 16:33:05 +0300

    Update clang-language-standard

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -197,7 +197,7 @@
 
 ;; Flycheck
 (add-hook 'after-init-hook #'global-flycheck-mode)
-(add-hook 'c++-mode-hook (lambda () (setq flycheck-clang-language-standard "c++14")))
+(add-hook 'c++-mode-hook (lambda () (setq flycheck-clang-language-standard "c++1z")))
 (add-hook 'org-mode-hook (lambda () (flycheck-mode -1)))
 
 ;; Rainbows!
