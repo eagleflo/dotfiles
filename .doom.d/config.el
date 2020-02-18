cc96af3da34864f0470210dda24cca544752a33d
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2020-02-18 02:30:54 +0200

    Try to persist frame position & size in Doom Emacs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -51,3 +51,23 @@
 ;;
 ;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
 ;; they are implemented.
+
+;; Own addition: persist the frame on quit!
+(when-let* ((dims (doom-cache-get 'last-frame-size)))
+  (cl-destructuring-bind ((left . top) width height fullscreen) dims
+    (setq initial-frame-alist
+          (append initial-frame-alist
+                  `((left . ,left)
+                    (top . ,top)
+                    (width . ,width)
+                    (height . ,height)
+                    (fullscreen . ,fullscreen))))))
+
+(defun save-frame-dimensions ()
+  (doom-cache-set 'last-frame-size
+                  (list (frame-position)
+                        (frame-width)
+                        (frame-height)
+                        (frame-parameter nil 'fullscreen))))
+
+(add-hook 'kill-emacs-hook #'save-frame-dimensions)
