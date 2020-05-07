bc3eb235a9b3e16e382892729a657b68575cbced
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2020-05-07 16:38:40 +0300

    Comment out persisting frame on quit for now

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -59,25 +59,25 @@
 (setq typescript-indent-level 2)
 (setq css-indent-offset 2)
 
-;; Persist the frame on quit!
-(when-let* ((dims (doom-cache-get 'last-frame-size)))
-  (cl-destructuring-bind ((left . top) width height fullscreen) dims
-    (setq initial-frame-alist
-          (append initial-frame-alist
-                  `((left . ,left)
-                    (top . ,top)
-                    (width . ,width)
-                    (height . ,height)
-                    (fullscreen . ,fullscreen))))))
+;; ;; Persist the frame on quit!
+;; (when-let* ((dims (doom-cache-get 'last-frame-size)))
+;;   (cl-destructuring-bind ((left . top) width height fullscreen) dims
+;;     (setq initial-frame-alist
+;;           (append initial-frame-alist
+;;                   `((left . ,left)
+;;                     (top . ,top)
+;;                     (width . ,width)
+;;                     (height . ,height)
+;;                     (fullscreen . ,fullscreen))))))
 
-(defun save-frame-dimensions ()
-  (doom-cache-set 'last-frame-size
-                  (list (frame-position)
-                        (frame-width)
-                        (frame-height)
-                        (frame-parameter nil 'fullscreen))))
+;; (defun save-frame-dimensions ()
+;;   (doom-cache-set 'last-frame-size
+;;                   (list (frame-position)
+;;                         (frame-width)
+;;                         (frame-height)
+;;                         (frame-parameter nil 'fullscreen))))
 
-(add-hook 'kill-emacs-hook #'save-frame-dimensions)
+;; (add-hook 'kill-emacs-hook #'save-frame-dimensions)
 
 ;; Start org-mode week properly
 (setq calendar-week-start-day 1)
