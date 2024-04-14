b3fe846e2823b96bd178b9377f1a4324f9c4de1e
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2024-04-14 17:59:55 +0300

    Quick draft of a script setting up macOS

---
+++ setup-mac.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# Appearance: Auto
+defaults write -globalDomain AppleInterfaceStyleSwitchesAutomatically -bool true
+
+# Dock > Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Key repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1
+
+# Delay until repeat
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
+
+# Applications
+# brew tap railwaycat/emacsmacport
+# brew install emacs-mac
+
+# brew tap homebrew/cask-fonts
+# brew install --cask font-fira-code
+
+# brew install fd fnm fzf git gpg htop mosh nvim ripgrep tig
+# brew install bat dust eza hyperfine tokei vivid zoxide
+# brew install go python3 ruby rustup
+
+# brew install --cask alacritty android-studio rectangle Spotify
